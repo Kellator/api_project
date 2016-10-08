928b53af91bbf63f42d22233268b83b913cb78bf
Kellator <k.macleod21@gmail.com>
2016-10-08 17:19:02 -0400

    apis and endpoints added

---
+++ app.js
@@ -0,0 +1,19 @@
+var igdb_base_url = "https://igdbcom-internet-game-database-v1.p.mashape.com"
+var youtube_base_url = "https://www.googleapis.com/youtube/v3/search"
+var twitch_base_url = "https://api.twitch.tv/kraken."
+
+var igdb_key = "65vlqIW8HRmshKsG0KIY236EqOB4p1OPDiYjsnO1DuCj7LGeYt"
+var youtube_key = "AIzaSyC8vg9sZggTKGAiYvX4wGs9H46pR2_spPM"
+var twitch_key = 
+
+var igdb_search
+
+var youtube_search_gameplay
+var youtube_search_walkthrough
+var youtube_search_commentary
+var youtube_channel = "/channel/"
+var youtube_video = "/watch?"
+
+var twitch_search_channels = "/search/channels?"
+var twitch_search_streams = "/search/streams?"
+var twitch_search_games = "/search/games?"
